7a98e359643f13ead4182c74e6ec287a6fb7cb44
mahekk <mahekk@sfu.ca>
2021-02-14 19:23:56 -0800

    refactor: use materialUI Button and add customization to it to fit figma design

--- frontend/components/MenuSideBar.tsx
+++ frontend/components/MenuSideBar.tsx
@@ -1,19 +1,57 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
+
+import { createMuiTheme, createStyles, withStyles, makeStyles, Theme, ThemeProvider, } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
-import Image from "next/image";
-import SvgIcon from "@material-ui/core/SvgIcon";
 import { Box, Icon } from "@material-ui/core";
+
+const BootstrapButton = withStyles({
+  root: {
+    boxShadow: 'none',
+    textTransform: 'none',
+    fontSize: 16,
+    padding: '20px',
+    border: '1px solid',
+    color: 'black',
+    width: '80%',
+    lineHeight: 0.8,
+    backgroundColor: 'white',
+    borderColor: 'none',
+    borderRadius: '999px',
+    margin: '20px 0',
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:hover': {
+      backgroundColor: '#8FC6F3',
+      borderColor: '#8FC6F3',
+      boxShadow: 'none',
+    },
+    '&:active': {
+      boxShadow: 'none',
+      backgroundColor: 'primary',
+      borderColor: '#005cbf',
+    },
+    '&:focus': {
+      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.5)',
+    },
+  },
+})(Button);
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
   backgroundGradient: {
-     background: "#FCA326",
+     background: "#187BCD",
   },
   card: {
      background: "white",
@@ -21,8 +59,20 @@ const useStyles = makeStyles((theme) => ({
   button: {
       margin: theme.spacing(1),
       borderRadius: "999"
-    },
+  },
+  sidebar__breakpoint__text: {
+    fontSize: '16px',
+    color: 'white',
+    marginTop: 0,
+    marginBottom: 0,
+  },
+  sidebar__breakpoint: {
+     backgroundColor: 'white',
+     width: '100%',
+     height: '2.5px',
+  },
 }));
+
 
 // TODO: need to make an API request (backend should have contributors: id, name, etc),
 // populate an array of names, sort names alphabetically and display them using a mapping
@@ -42,16 +92,15 @@ const MenuSideBar = () => {
         alignItems="center"
         >
 
-        <button className="sidebar__button sidebar__button--active" onclick="general overview">
+        <BootstrapButton variant="contained" color="primary" disableRipple className={classes.margin}>
             Everyone
-        </button>
+        </BootstrapButton>
 
-        <p className="sidebar__breakpoint__text">Contributors</p>
-        <div className="sidebar__breakpoint"></div>
-
-        <button className="sidebar__button" >
+        <p className={classes.sidebar__breakpoint__text}>Contributors</p>
+        <div className={classes.sidebar__breakpoint}></div>
+        <BootstrapButton variant="contained" color="primary" disableRipple className={classes.margin}>
             memberName
-        </button>
+        </BootstrapButton>
       </Box>
   );
 };
