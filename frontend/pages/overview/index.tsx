7e1c792f8a461b60617d169c2252f96aae17994c
mahekk <mahekk@sfu.ca>
2021-02-17 22:38:13 -0800

    feat: add base of code analysis page

---
+++ frontend/pages/overview/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "../../components/NavBar";
+import MenuSideBar from "../../components/MenuSideBar";
+
+const index = () => {
+    return (
+        <>
+        <NavBar/>
+        <MenuSideBar/>
+        </>
+    );
+};
+
+export default index;
